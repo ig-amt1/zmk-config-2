5cc3174107b42a812881bb795a6ea01ee73dfbe8
Cássio Ávila <cassioavila@protonmail.com>
2022-12-03 22:39:41 -0300

    tests

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "zmk config for the TOTEM keyboard";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { utils, nixpkgs }: {
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
